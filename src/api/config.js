7b7385e549aa4d15d918ecc74abf1009abf12ede
Yomar Miranda <mirandayomar@gmail.com>
2017-12-13 21:21:35 -0500

    Ahora lo que hacemos es exportar un objeto con los datos de la configuración.

--- src/api/config.js
+++ src/api/config.js
@@ -1,7 +1,6 @@
-
-/**
-	Application name: 	platzi-music
-	API key:			bbcc65dcdd022f93494803676b93aa21
-	Shared secret:		5114e66b08c912dedab0256a6f2ec52f
-	Registered to:		yomar_dev
-*/
+export default{
+	appName: 'platzi-music',
+	apiKey: 'bbcc65dcdd022f93494803676b93aa21',
+	secret: '5114e66b08c912dedab0256a6f2ec52f',
+	registeredTo: 'yomar_dev'
+}
